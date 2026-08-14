108b7a0bbd30c4cf381321886d0e4be37ef7bc92
eeeiqjjj <eeeiqjjj@gmail.com>
2026-08-14 09:27:18 -0400

    Update ege.lua

--- ege.lua
+++ ege.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/38b5c9a9fe8ab4204d90c0040b63f6b5b1dae9d8708c0c734d61a125b43c4ba4/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/3f30863a68593f7fdf3939318704dde2bacb03323fd5d5e02553342f9fa30344/download"))()
